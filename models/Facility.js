040da6c06169ce794212c5804adbdfcf364ce974
Christopher Hutchinson <cshutchinson@gmail.com>
2015-04-13 13:14:05 -0400

    added facility schema model

---
+++ models/Facility.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var facilitySchema = new mongoose.Schema({
+    facilityName: String,
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    contactName: String,
+    contactPhone: String,
+    contactEmail: String,
+    buildingName: String,
+    locationName: String,
+    securityNeeded: String,
+    securityQty: Number,
+    peformanceAreaLength: Number,
+    performanceAreaWidth: Number,
+    waiverNeeded: Boolean,
+    waiverURL: String,
+    performanceAreaCapacity: Number,
+    isActive: Boolean,
+    approved: Boolean,
+    approvedBy: String,
+    previousEngagements: Number,
+    canceledEngagements: Number,
+    notes: Array
+});
+
+module.exports = mongoose.model('Facility', facilitySchema);
