e07da59ed0fda2131cbeecc4999504a3e01ed718
Joel Bentley <jbben23@gmail.com>
2015-04-27 17:48:34 -0400

    removed Delete Account from profile page, removed footer from account home pages

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,19 @@
  * Home page.
  */
 exports.index = function(req, res) {
+
+  if (req.user) {
+    if (req.user.accountType==='Musician') {
+      res.redirect('/homeMusician');
+    } else if (req.user.accountType==='Facility') {
+      res.redirect('/homeFacility');
+    } else if (req.user.accountType==='Admin') {
+      res.redirect('/homeAdmin');
+    } else {
+      res.redirect('/');
+    }
+  }
+
   res.render('home', {
     title: 'Home'
   });
