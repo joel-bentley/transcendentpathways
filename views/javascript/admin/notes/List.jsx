c10e8fddfca80351cba4982e3293aa56cbde5730
Christopher Hutchinson <cshutchinson@gmail.com>
2015-05-07 23:18:48 -0400

    change layout of note

--- views/javascript/admin/notes/List.jsx
+++ views/javascript/admin/notes/List.jsx
@@ -8,47 +8,49 @@ var List = React.createClass({
                 listStyleType: "none"
             },
             listGroup: {
-                margin: '5px 5px',
-                borderRadius: 10
+                margin: '10px 10px',
+                borderRadius: 5
             },
             removeItem: {
-                fontSize: 20,
+                fontSize: 14,
                 float: "left",
                 position: "absolute",
-                top: 12,
-                left: 6,
+                top: 4,
+                left: 4,
                 cursor: "pointer",
-                color: "rgb(0, 0, 255)"
+                color: "rgb(255, 0, 0)"
             },
             todoItem: {
+                top:2,
                 paddingLeft: 20,
                 fontSize: 14
             },
             todoItemDate: {
                 paddingLeft: 20,
                 float: "right",
-                fontSize: 12
+                fontSize: 14,
+                bottom: 5
             }
         };
         var timeDate = new Date();
         var listItems = this.props.items.map(function(item, index){
             return(
                 <li className = "list-group-item" style = {styles.listGroup} key={this.props.name+item}>
-                        <span
-                        className = "glyphicon glyphicon-remove"
-                        style = {styles.removeItem}
-                        onClick =  {this.props.remove.bind(null, index)}>
-                        </span>
-                    <div >
-                        <span style = {styles.todoItem}>
-                            {item}
-                        </span>
-                    </div>
-                    <div >
-                        <span style = {styles.todoItemDate}>
-                            {timeDate.toDateString()}
-                        </span>
-                    </div>
+                    <span
+                    className = "glyphicon glyphicon-remove"
+                    style = {styles.removeItem}
+                    onClick =  {this.props.remove.bind(null, index)}>
+                    </span>
+
+                    <span style = {styles.todoItem}>
+                        {item}
+                    </span>
+
+
+                    <span style = {styles.todoItemDate}>
+                        {timeDate.toDateString()}
+                    </span>
+
                 </li>
             )
         }.bind(this));
