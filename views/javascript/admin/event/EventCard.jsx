7e6c86942447b01d2b9f4d866f31e03a0d37468f
Christopher Hutchinson <cshutchinson@gmail.com>
2015-05-14 21:38:26 -0400

    events list working facility name and number added

--- views/javascript/admin/event/EventCard.jsx
+++ views/javascript/admin/event/EventCard.jsx
@@ -29,16 +29,22 @@ var EventCard = React.createClass({
                 <div className="panel-body">
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-sm-6">
+                            <div className="col-xs-6">
                                 <h5>Start: {new Date(this.props.event.startTime).toLocaleTimeString()}</h5>
                             </div>
-                            <div className="col-sm-6">
+                            <div className="col-xs-6">
                                 <h5>End: {new Date(this.props.event.endTime).toLocaleTimeString()}</h5>
                             </div>
                         </div>
                         <div className="row">
-                            <h5>{this.props.facility.contactName}{this.props.facility.contactPhone}</h5>
-
+                            <div className="col-xs-10 col-xs-offset-1">
+                                <h5>Facility Contact {this.props.facility.contactName}</h5>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-3">
+                                <h5>Phone {this.props.facility.contactPhone}</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
