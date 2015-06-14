ad320862191deb63d4b83ab1b2b857a4722aa0df
Christopher Hutchinson <cshutchinson@gmail.com>
2015-06-14 02:51:17 -0400

    more improvements to musician main page

--- views/javascript/musician/EventRow.jsx
+++ views/javascript/musician/EventRow.jsx
@@ -29,10 +29,21 @@ var EventRow = React.createClass({
                                     null}
                                 </div>
                                 </h5>
+
                             </div>
                             <div id="map-canvas">
                                 {this.props.musician && this.props.facility ? <GoogleMap musician={this.props.musician} facility={this.props.facility}/>: null}
                             </div>
+                            <row>
+                                <div className="col-sm-12">
+                                    <div className="col-sm-5">
+                                    <p className="text-center"> * Request does not an any way imply that you will be approved for this event!</p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-6 col-sm-offset-1">
+                                    <button className="btn btn-danger" type="submit">Request Event  *</button>
+                                </div>
+                            </row>
                         </div>
                     </div>
                 </div>
