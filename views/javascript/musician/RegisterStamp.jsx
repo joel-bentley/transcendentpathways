357bc2af204d36457167b52d80548c6d3ce28aea
Christopher Hutchinson <cshutchinson@gmail.com>
2015-06-16 19:26:03 -0400

    Streamlined some code, removed style from musician table

---
+++ views/javascript/musician/RegisterStamp.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var RegisterStamp = React.createClass({
+
+   render: function(){
+
+       return (
+           <div>
+
+           </div>
+       )
+   }
+
+});
+
+module.exports = RegisterStamp;
